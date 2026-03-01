691783717e631065134f54737287b05633e53d1c
Aline Fernandes <fernandes.dev@outlook.com>
2026-03-01 17:29:01 -0300

    add aula de declarando variáveis

---
+++ src/DeclarandoVariaveis.java
@@ -0,0 +1,21 @@
+public class DeclarandoVariaveis {
+    public static void main(String[] args){
+
+        //tipos de dados
+/*
+byte idade = 22, quantidadeDeDependentes = 1;
+short ano = 2000, mes = 5, dia = 15;
+int quantidade = , itens;
+long numeroDeVendas, colaboradores;
+float altura = 1.78f, largura = 0.65f, peso = 75.5f, aliquota = 0.38f;
+double preco = 1.99;
+boolean ativo, maior;
+char sexo;
+String nome;
+*/
+        byte idade = 22, quantidadeDeDependentes = 1;
+        long numeroDeVendas, colaboradores;
+        float altura = 1.78f, largura = 0.65f, peso = 75.5f, aliquota = 0.38f;
+        double preco = 19.99;
+    }
+}
