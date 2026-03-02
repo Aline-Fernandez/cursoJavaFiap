d5d509962cad2d52518c0881c5c8661c9fc68cbb
Aline Fernandes <fernandes.dev@outlook.com>
2026-03-02 12:34:33 -0300

    add operadores aritmeticos

---
+++ src/OperadorAritmetico.java
@@ -0,0 +1,28 @@
+public class OperadorAritmetico {
+    public static void main(String[] args) {
+
+        //exemplo  -
+        //System.out.println("5/2 = " + 5f/2f);
+
+//exemplo 2 -
+// var numero = 1450;
+//        System.out.println("O número " + numero + "é divisível por 2 " + (numero % 2 == 0));
+//        System.out.println("O número " + numero + "é divisível por 3 " + (numero % 3 == 0));
+//        System.out.println("O número " + numero + "é divisível por 4 " + (numero % 4 == 0));
+//        System.out.println("O número " + numero + "é divisível por 5 " + (numero % 5 == 0));
+//        System.out.println("O número " + numero + "é divisível por 6 " + (numero % 6 == 0));
+//        System.out.println("O número " + numero + "é divisível por 7 " + (numero % 7 == 0));
+//        System.out.println("O número " + numero + "é divisível por 8 " + (numero % 8 == 0));
+//        System.out.println("O número " + numero + "é divisível por 9 " + (numero % 9 == 0));
+
+        byte quantidadeDeUvasVerdes = 50;
+
+        byte quantidadeDeUvasRoxas = 120;
+
+        int totalDeUvas = quantidadeDeUvasVerdes + quantidadeDeUvasRoxas;
+
+        System.out.println(totalDeUvas);
+
+
+    }
+}
