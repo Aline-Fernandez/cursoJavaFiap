93117ec79fd9b46ce6f0a2421dffa0984e78a5f2
Aline Fernandes <fernandes.dev@outlook.com>
2026-03-03 16:39:04 -0300

    add exercicío de fixação

---
+++ src/ExercicioDeFixacao.java
@@ -0,0 +1,10 @@
+public class ExercicioDeFixacao {
+    public static void main(String[] args) {
+
+int ps = 7, tcc = 5, av1 = 10, av2 = 6;
+
+    double mediaFinal = ps * 0.5 + tcc * 0.3 + ((av1 + av2) /2) * 0.2;
+
+        System.out.println("A media final é: " + mediaFinal);
+    }
+    }
